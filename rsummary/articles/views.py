e997c838583cbcd75c8391b67e60952e654bb68b
ryusangwon <r8uuuuuuuu@gmail.com>
2022-07-01 23:50:30 +0900

    use urls

--- rsummary/articles/views.py
+++ rsummary/articles/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return render(request, 'articles/index.html')
+    today = datetime.today().date()
+    context = {"date":today}
+    return render(request, 'articles/index.html', context=context)
