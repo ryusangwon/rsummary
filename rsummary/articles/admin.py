08e73ac40efdc946c11d89d9d958d8c340624097
ryusangwon <r8uuuuuuuu@gmail.com>
2022-07-04 21:41:58 +0900

    fixed untracked files

--- rsummary/articles/admin.py
+++ rsummary/articles/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+# 추가해줄 모델 가져오기
 # Register your models here.
+
+# admin.site.register(추가할 모델)
--- rsummary/articles/models.py
+++ rsummary/articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+# model 생성 후 python manage.py makemigrations를 통해 알려주어야한다. model의 변경사항 인식
+# python manage.py migrate 을 통해 실제 데이터베이스 적용.
+
 # Create your models here.
+# class Menu(models.Model):
+#     name = models.CharField(max_length=50)
+#     description = models.CharField(max_length=100)
+#     price = models.IntegerField()
+    
+#     def __str__(self):
+#         return self.name
