f499192a903f8cb9fa1979f556a9e7bfe808047e
ryusangwon <r8uuuuuuuu@gmail.com>
2022-07-08 17:20:30 +0900

    Modify urls

--- rsummary/articles/views.py
+++ rsummary/articles/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import datetime
+from .models import Article
 
 # Create your views here.
-def index(request):
-    today = datetime.today().date()
-    context = {"date":today}
-    return render(request, 'articles/index.html', context=context)
+def article_list(request):
+    articles = Article.objects.all()
+    context = {}
+    context['articles'] = articles
+    return render(request, 'articles/article_list.html', context=context)
+
+def article_detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    context = {}
+    context['article'] = article
+    return render(request, 'articles/article_detail.html', context=context)
